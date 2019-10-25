fb7d788bd1dd5ea2dfbc58126e6610edfdeb3344
mgw0710 <56991120+mgw0710@users.noreply.github.com>
2019-10-25 13:50:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Welcome to my website!<br/>");
+    response.write("<a href='/.auth/login/microsoftaccount'>Log in with Microsoft Account</a>");
+    response.end();
+
 });
 
 const port = process.env.PORT || 1337;
